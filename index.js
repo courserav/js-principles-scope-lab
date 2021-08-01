3760ad57903f02bf705bd8fea9c4ed6d655a9a0a
courserav <lvsmlit@gmail.com>
2021-08-01 14:03:25 -0400

    commit tree 1f696e301925178ada4e341c6decc7c039fb935f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "boberino"
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bobby hill"
+}
